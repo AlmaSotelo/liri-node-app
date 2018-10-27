c60dd03b8b154481e45525752f0805afb2068423
Alma@github.noreply.github.com <almatcss@yahoo.com>
2018-10-27 00:58:34 -0700

    final corrected-testing screenshot

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # liri-node-app
  LIRI is a command line node app that takes in parameters and gives you back data.
+ The user have the following options to search:
+    1. spotify-this-song / song name 
+       Output:
+             Song name
+             Preview URL
+             Album name
+             Artist name
+    2. concert-this / artist or band name
+       Output:
+             Venue name
+             Location (city,state)
+             Date (DD MM YYYY)
+    3. movie-this / movie name
+       Output:
+             Title of the movie
+             Year the movie came out
+             IMDB Rating of the movie
+             Rotten Tomatoes Rating of the movie
+             Country where the movie was produced
+             Language of the movie
+             Plot of the movie
+             Actors in the movie
+    4. do-what-it says
+        Output:
+             A random song. Also the code for a random concert and a random movie is included as a commented code.
+             ![image1](/images/spotify-this-song)
+   
+
+ Spotify-this-song is coded using npm Node-Spotify-API, and keys used are hided.
+ concert-this and movie-this uses the 'require".
+
+ 
+
+
--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 var request = require("request");
 var moment = require('moment');
 var Spotify = require('node-spotify-api');
+// fs is a core Node package for reading and writing files
 var fs = require('fs');
 
 // to import the `keys.js` file and store it in a variable.
@@ -60,7 +61,7 @@ else if (command == "concert-this"){
       var jsonBody = JSON.parse(body);
       //  console.log(JSON.stringify(jsonBody,null,2));
       for (i=0; i<jsonBody.length; i++) {
-        console.log("Veneu Name : " + JSON.parse(body)[i].venue.name);
+        console.log("Venue Name : " + JSON.parse(body)[i].venue.name);
         city = JSON.parse(body)[i].venue.city;
         state= JSON.parse(body)[i].venue.region;
         console.log("Location : " + city + ", " + state);
@@ -97,7 +98,71 @@ else if (command === "movie-this"){
   }); 
 }
 else if (command === "do-what-it-says"){       //  to look for "do-what-it-says"
-  console.log(fs.readFileSync("random.txt").toString());
+  // This block of code will read from the "random.txt" file.
+  // It's important to include the "utf8" parameter or the code will provide stream data (garbage)
+  // The code will store the contents of the reading inside the variable "data"
+  fs.readFile("random.txt", "utf8", function(error, data) {
+  // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+    // split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+    
+    var song = dataArr[1];
+    spotify.search({ type: 'track', query: song }, function(err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      //console.log(data.tracks.items[0]); 
+      console.log("Song Name : " + data.tracks.items[0].name);
+      console.log("Preview URL : " + (data.tracks.items[0].preview_url ? data.tracks.items[0].preview_url : "Not Available") );
+      console.log("Album Name: " + data.tracks.items[0].album.name);
+      console.log("Artist Name: " + data.tracks.items[0].album.artists[0].name );
+      });
+    
+                 //  movie
+    // var movie = dataArr[1];
+    // queryUrl = "http://www.omdbapi.com/?apikey=trilogy&t=" + movie;   
+    // request(queryUrl, function(error, response, body) {
+    //   // If the request was successful...
+    //   if (!error && response.statusCode === 200) {
+    //   console.log("Title : " + JSON.parse(body).Title );
+    //   console.log("The movie came out in " + JSON.parse(body).Year);
+    //   console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+    //   console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+    //   console.log("Produced in: " + JSON.parse(body).Country);
+    //   console.log("Language: " + JSON.parse(body).Language);
+    //   console.log("Plot: " + JSON.parse(body).Plot);
+    //   console.log("Actors: "+ JSON.parse(body).Actors);
+    //   }
+    // });
+      
+              //  concert
+    // var artist = dataArr[1];            
+    // queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+    // request(queryUrl, function(error, response, body) {
+    //   // If the request was successful...
+    //   if (!error && response.statusCode === 200) {
+    //     // Parse the body of the site and recover just what is needed
+    //     var jsonBody = JSON.parse(body);
+    //     //  console.log(JSON.stringify(jsonBody,null,2));
+    //     for (i=0; i<jsonBody.length; i++) {
+    //       console.log("Venue Name : " + JSON.parse(body)[i].venue.name);
+    //       city = JSON.parse(body)[i].venue.city;
+    //       state= JSON.parse(body)[i].venue.region;
+    //       console.log("Location : " + city + ", " + state);
+    //       date = moment(JSON.parse(body)[i].datetime).format('DD MM YYYY');
+    //       console.log("Date: " + date);
+    //       console.log("**********************************");
+    //     }
+    //   } 
+    // }); 
+  
+  });
+
 };
+
+
 
 
